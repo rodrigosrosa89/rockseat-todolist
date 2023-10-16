4047c53712141fd624db8deb97b4263d7e0384b5
Rodrigo Rosa <rodsiqueirabh@hotmail.com>
2023-10-16 00:10:02 -0300

    first commit

---
+++ src/main/java/br/com/rodao/rockseattodolist/RockseatTodolistApplication.java
@@ -0,0 +1,13 @@
+package br.com.rodao.rockseattodolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RockseatTodolistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RockseatTodolistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/rodao/rockseattodolist/RockseatTodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rodao.rockseattodolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RockseatTodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
